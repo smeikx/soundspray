722f4c173801fea0f478bcf6faba6cdd2b05eccc
smeikx <gthb@smeikx.com>
2018-11-10 10:17:32 +0100

    lege Grundlage für serielle Kommunikation

--- soundspray/soundspray.ino
+++ soundspray/soundspray.ino
@@ -2,36 +2,39 @@ using Pin = unsigned int;
 using Milliseconds = unsigned long;
 
 const Pin tilt_sensor {8}, release_button {7};
-bool last_state;
-int count {0}; 
-const unsigned int decrease_rate {5}; // per second
+bool last_shake_state, release_button_previously_pressed;
+
+enum MESSAGE
+{
+	CHARGE_UP,
+	RELEASE_START,
+	RELEASE_STOP
+};
 
 void setup()
 {
 	Serial.begin(9600);
 	pinMode(tilt_sensor, INPUT);
 	pinMode(release_button, INPUT);
-	last_state = digitalRead(tilt_sensor);
+	last_shake_state = digitalRead(tilt_sensor);
+	release_button_previously_pressed = digitalRead(release_button);
 }
 
 void loop()
 {
-	if (digitalRead(release_button))
-		count = 0;
-
-	bool state {digitalRead(tilt_sensor)};
-	if (state != last_state)
+	bool release_button_currently_pressed {digitalRead(release_button)};
+	if (release_button_currently_pressed != release_button_previously_pressed)
 	{
-		count++;
-		last_state = state;
-		Serial.println(count);
+		Serial.write(release_button_currently_pressed ? RELEASE_START : RELEASE_STOP);
+		release_button_previously_pressed = release_button_currently_pressed;
 	}
 
-	count -= delta_time() / 1000.0f * decrease_rate;
-	count = max(count, 0);
-
-	Serial.write(count);
-	delay(10);
+	bool state {digitalRead(tilt_sensor)};
+	if (state != last_shake_state)
+	{
+		last_shake_state = state;
+		Serial.write(CHARGE_UP);
+	}
 }
 
 // misst vergangene Zeit seit letztem Aufruf
